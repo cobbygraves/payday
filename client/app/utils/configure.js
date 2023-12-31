2b294ddae7ceabcaec139097eb408f591fb304d8
cobbygraves <rmeocnis@hotmail.com>
2023-12-31 09:14:10 +0000

    Database URL Changed

--- client/app/utils/configure.js
+++ client/app/utils/configure.js
@@ -1,2 +1,2 @@
 // export const hostAPI = 'http://localhost:5000'
-export const hostAPI = 'https://paydaygh-7360d25bb576.herokuapp.com/'
+export const hostAPI = 'https://paydaygh-7360d25bb576.herokuapp.com'
